c1cf1e3fcb4d488280424d072a2355438789b958
sunny-logic <vk.sanmati@gmail.com>
2023-04-26 10:38:43 +0100

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo "Hello World, My Name is Prish, Time is `date` and it's a rainy day today" >> /tmp/log_file.log
+echo "Hello World, My Name is `whoami`, Time is `date` and it's a rainy day today"
